f6013e272c89eff2e8f6fa9f166514ad06043457
SaranRaj <saranraj.1803164@srec.ac.in>
2024-12-05 13:22:50 +0530

    Footer component created and complete home page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Footer from '../Components/Footer/Footer'
 
 const shop = () => {
   return (
@@ -13,6 +14,7 @@ const shop = () => {
       <Offers/>
       <NewCollections/>
       <NewsLetter/>
+     
      
     </div>
   )
