97881a8fc627c3eb59bb49643e5856d44ac40e4e
SaranRaj <saranraj.1803164@srec.ac.in>
2024-12-07 12:59:52 +0530

    contex and shopcatory page created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import ShopContextProvider from './Context/ShopContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ShopContextProvider>
+     <App />
+  </ShopContextProvider>
+  
+); 
